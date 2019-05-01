e7b2e8e9f4055005b770a7df8e9e0fb1d317cd16
Newforce <nicholasandrewmitchell@gmail.com>
2019-04-30 22:29:33 -0400

    v.1.0.6.0

--- src/components/player.js
+++ src/components/player.js
@@ -4,10 +4,33 @@ import { Link } from "react-router-dom";
 
 class Characters extends Component {
   state = {
-    playerName: "Nick",
-    background:
-      "Apollyon began his career as a guardian angel over a century ago. That path changed when the one he was trusted to guard turned to a life of crime. Without a job he fell into the role of an oathbreaker, bringing destruction to the world under the employ of Asmodeus. This relationship prospered and grew until he became a chosen of Asmodeus. Now he travels the planes completing quests as they are given, but unknown to everyone he has a secret quest of his own..."
   };
+
+  weightLimits = () => {
+    if (this.props.currentWeight < this.props.maxCarry / 2) {
+      const normal = (
+        <p className="lightWeight">
+          (Weight: {this.props.currentWeight}/{this.props.maxCarry} lbs.)
+        </p>
+      );
+      return normal;
+    } else if (this.props.currentWeight <= this.props.maxCarry) {
+      const slow = (
+        <p className="heavyWeight">
+          (Weight: {this.props.currentWeight}/{this.props.maxCarry} lbs.)
+        </p>
+      );
+      return slow;
+    } else if (this.props.currentWeight > this.props.maxCarry) {
+      const stopped = (
+        <p className="maxWeight">
+          (Weight: {this.props.currentWeight}/{this.props.maxCarry} lbs.)
+        </p>
+      );
+      return stopped;
+    }
+  };
+
   render() {
     return (
       <div className="playerPage">
@@ -29,9 +52,10 @@ class Characters extends Component {
             <hr className="my-4" />
             <h4 className="color-white blue-background-player">
               Items{" "}
-              <p>
+              {/* <p className="weight">
                 (Weight: {this.props.currentWeight}/{this.props.maxCarry} lbs.)
-              </p>
+              </p> */}
+              {this.weightLimits()}
             </h4>
             <p className="thin-line" />
             <div className="items">
